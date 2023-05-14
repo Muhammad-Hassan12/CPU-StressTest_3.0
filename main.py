cefbd52714dd24b62d772601dc5ec47f8483696c
S.M Hassan <118466320+Muhammad-Hassan12@users.noreply.github.com>
2023-05-15 00:26:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,16 @@
 import numpy as np
 import time
 
-# Define the size of the matrix to multiply
 MATRIX_SIZE = 1000
 
-# Create two random matrices of the same size
 matrix1 = np.random.rand(MATRIX_SIZE, MATRIX_SIZE)
 matrix2 = np.random.rand(MATRIX_SIZE, MATRIX_SIZE)
 
 start_time = time.time()
 
-# Perform matrix multiplication repeatedly in a loop
 while True:
     np.dot(matrix1, matrix2)
 
-    # Check if the desired time has elapsed
     elapsed_time = time.time() - start_time
-    if elapsed_time >= 60:  # Change the time limit as needed
+    if elapsed_time >= 60:
         break
